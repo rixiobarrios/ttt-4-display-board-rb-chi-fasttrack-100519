d004d1adbf8b7cabb70dc7f31a5ccd831233f21c
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-05 20:32:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,6 @@ puts " #{board[0]} | #{board[1]}  | #{board[2]} "
 puts "------------"
 puts "   | #{board[4]} |   "
 puts "------------"
-puts "   |    |   "
+puts " #{board[6]}  | #{board[7]}  | #{board[0]} "
 
 end
